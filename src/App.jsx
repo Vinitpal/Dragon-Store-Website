5306075b27b4e574293ab8247e014a50096f6009
Vinit <vs361017@gmail.com>
2021-07-10 13:26:58 +0530

    fixed ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import ProductPage from "./components/product-page/ProductPage";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage";
-// import CartSection from "./components/cart-section/CartSection";
+import CartSection from "./components/cart-section/CartSection";
 
 const App = () => {
   return (
     <div className="app">
       <Switch>
         <Route path="/product" component={ProductPage} />
+        <Route path="/cart" component={CartSection} />
         <Route component={HomePage} />
       </Switch>
     </div>
--- src/components/navbar/Rightnav.jsx
+++ src/components/navbar/Rightnav.jsx
@@ -1,20 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 const Rightnav = () => {
   return (
     <div className="right-nav">
-      <div className="cart">
-        <i class="far fa-shopping-cart"></i>
-        <span className="cart-count">0</span>
-      </div>
+      <NavLink to="/cart">
+        <div className="cart">
+          <i class="far fa-shopping-cart"></i>
+          <span className="cart-count">0</span>
+        </div>
+      </NavLink>
       <div className="username-wrapper">
         <h3>Vinitpal</h3>
+      </div>
+      <div className="log-out">
         <p>Log Out</p>
       </div>
-      <div className="pfp">
+      {/* <div className="pfp">
         <img
           src="https://lh3.googleusercontent.com/-fSyU5_zMCUc/XXPnLIfIYVI/AAAAAAAAB0s/wknHnOv-tAw7iLAmQD_zEfEd7KdbjSpSACEwYBhgL/w140-h140-p/2019-09-07.jpg"
           alt="pfp"
         ></img>
-      </div>
+      </div> */}
     </div>
   );
 };
